e47fdae43d5f27020b7a3fa27b67876bb445613a
Frederico Berchof <fred.berchof@gmail.com>
2021-07-28 12:56:15 -0300

    Delete component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -28,6 +28,7 @@ import { MatInputModule } from "@angular/material/input";
 import { ProductReadComponent } from "./components/product/product-read/product-read.component";
 import { ProductRead2Component } from "./components/product/product-read2/product-read2.component";
 import { ProductUpdateComponent } from "./components/product/product-update/product-update.component";
+import { ProductDeleteComponent } from "./components/product/product-delete/product-delete.component";
 import { MatTableModule } from "@angular/material/table";
 import { MatPaginatorModule } from "@angular/material/paginator";
 import { MatSortModule } from "@angular/material/sort";
@@ -50,8 +51,8 @@ registerLocaleData(localePt)
     ProductCreateComponent,
     ProductReadComponent,
     ProductRead2Component,
-    ProductUpdateComponent
-
+    ProductUpdateComponent,
+    ProductDeleteComponent
   ],
   imports: [
     BrowserModule,
--- frontend/src/app/components/product/product.service.ts
+++ frontend/src/app/components/product/product.service.ts
@@ -1,14 +1,14 @@
-import { Product } from './product.module';
+import { Product } from "./product.module";
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { MatSnackBar } from "@angular/material/snack-bar";
-import { Observable } from 'rxjs';
+import { Observable } from "rxjs";
 
 @Injectable({
   providedIn: "root",
 })
 export class ProductService {
-  baseUrl = ' http://localhost:3001/products';
+  baseUrl = " http://localhost:3001/products";
 
   constructor(private snackBar: MatSnackBar, private http: HttpClient) {}
 
@@ -21,20 +21,25 @@ export class ProductService {
   }
 
   create(product: Product): Observable<Product> {
-    return this.http.post<Product>(this.baseUrl, product)
+    return this.http.post<Product>(this.baseUrl, product);
   }
 
-  read(): Observable<Product[]>{
-    return this.http.get<Product[]>(this.baseUrl)
+  read(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.baseUrl);
   }
 
-  readById(id: string | null): Observable<Product>{
-    const url = `${this.baseUrl}/${id}`
-    return this.http.get<Product>(url)
+  readById(id: string | null): Observable<Product> {
+    const url = `${this.baseUrl}/${id}`;
+    return this.http.get<Product>(url);
   }
 
-  update(product: Product): Observable<Product>{
-    const url = `${this.baseUrl}/${product.id}`
-    return this.http.put<Product>(url, product)
+  update(product: Product): Observable<Product> {
+    const url = `${this.baseUrl}/${product.id}`;
+    return this.http.put<Product>(url, product);
+  }
+
+  deleteProduct(id: number | undefined): Observable<Product> {
+    const url = `${this.baseUrl}/${id}`;
+    return this.http.delete<Product>(url);
   }
 }
